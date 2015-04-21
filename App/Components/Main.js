d77c41ffdc69481468be88cf7c68c3202b42b779
Jakša Bašić <jaksabasic1@gmail.com>
2015-04-21 11:18:26 +0200

    Main | Search Github Users View

---
+++ App/Components/Main.js
@@ -0,0 +1,95 @@
+var React = require('react-native');
+
+var {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableHighlight,
+    ActivityIndicatorIOS
+} = React;
+
+var style = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        padding: 30,
+        marginTop: 55,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#48BBeC'
+    },
+    title:{
+        marginBottom: 20,
+        fontSize: 25,
+        textAlign: 'center',
+        fontSize: 25,
+        color:'#fff'
+    },
+    searchInput:{
+        height: 50,
+        padding: 4,
+        marginRight: 5,
+        fontSize: 23,
+        color: 'white',
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 8
+    },
+    button:{
+        height: 45,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 10,
+        marginTop: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+    },
+    buttonText:{
+        fontSize: 18,
+        color: '#111',
+        alignSelf: 'center'
+    }
+});
+class Main extends React.Component{
+    constructor(props){
+       super(props);
+       this.state = {
+           username: '',
+           isLoading: false,
+           error: false
+       }
+    }
+    handleChange(event){
+       this.setState({
+           username: event.nativeEvent.text
+       });
+    }
+    handleSubmit(){
+        //Update for ActivityIndicatorIOS spinner
+        this.setState({
+            isLoading: true
+        });
+    }
+    render(){
+        return (
+           <View style={style.mainContainer}>
+               <Text style={style.title}>Search Github User</Text>
+               <TextInput
+                   style={style.searchInput}
+                   value={this.state.username}
+                   onChange={this.handleChange.bind(this)}/>
+               <TouchableHighlight
+                   style={style.button}
+                   onPress={this.handleSubmit.bind(this)}
+                   underlayColor="white">
+                   <Text style={style.buttonTest}>SEARCH</Text>
+               </TouchableHighlight>
+           </View>
+        )
+    }
+}
+
+module.exports = Main;
--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,34 @@
 'use strict';
 
 var React = require('react-native');
+var Main = require('./App/Components/Main')
 var {
   AppRegistry,
   StyleSheet,
+  NavigatorIOS,
   Text,
   View,
 } = React;
 
+var styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#111111'
+    }
+});
+
 class gitHubProfiler extends React.Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome Dude!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+          style ={styles.container}
+          initialRoute={{
+              title: 'Github Profiler',
+              component: Main
+          }} />
     );
   }
 }
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
 
 AppRegistry.registerComponent('gitHubProfiler', () => gitHubProfiler);
